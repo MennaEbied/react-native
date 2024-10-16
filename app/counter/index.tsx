aa919c4d1a849b31ce0661de2f53c25661946eba
MennaEbied <mennahesham253@gmail.com>
2024-10-16 21:47:31 +0300

    Add local push notifications

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,10 +1,38 @@
-import { Text, View, StyleSheet } from "react-native";
-
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import { theme } from "../../theme";
+import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
+import * as Notifications from "expo-notifications";
 
 export default function CounterScreen() {
+  const scheduleNotification = async () => {
+      const result = await registerForPushNotificationsAsync();
+      if(result==="granted"){
+        await Notifications.scheduleNotificationAsync({
+          content:{
+            title:"I'm a notification from your app"
+          }, 
+          trigger:{
+            seconds:5,
+          }
+        })
+      }else{
+        Alert.alert(
+          "Unable to schedule notification",
+          "Enable the notification permission for expo go in settings"
+          )
+      }
+  
+    };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Counter</Text>
+      <TouchableOpacity
+        onPress={scheduleNotification} 
+        style={styles.button} 
+        activeOpacity={0.8}
+      >
+        <Text style={styles.buttonText}>Schedule notification</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -19,4 +47,15 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 24,
   },
+  button:{
+    backgroundColor:theme.colorBlack,
+    padding:12,
+    borderRadius:6,
+  },
+  buttonText:{
+    fontWeight:"bold",
+    textTransform:"uppercase",
+    color:theme.colorWhite,
+    letterSpacing:1,
+  }
 });
