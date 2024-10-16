5d1a3907318d8d9100ac2886196ce933c2a3024f
MennaEbied <mennahesham253@gmail.com>
2024-10-16 18:36:44 +0300

    Use LayoutAnimation

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,11 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, TextInput ,FlatList, View,Text } from 'react-native';
+import { 
+  StyleSheet,
+   TextInput ,
+   FlatList, 
+   View,Text,
+   LayoutAnimation 
+} from 'react-native';
 import { ShoppingListItem } from '../components/ShoppingListItem';
 import { theme } from '../theme';
 import { getFromStorage, saveToStorage } from '../utils/storage';
@@ -20,6 +26,7 @@ export default function App() {
     const fetchInitial=async()=>{
       const data= await getFromStorage(storageKey)
       if(data){
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
         setShoppingList(data)
       }
     }
@@ -36,6 +43,7 @@ export default function App() {
         },
         ...shoppingList
       ];
+      LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
       setShoppingList(newShoppingList);
       saveToStorage(storageKey,newShoppingList);
       setValue("");
@@ -46,6 +54,7 @@ export default function App() {
   const handleDelete=(id:string)=>{
     const newShoppingList=shoppingList.filter((item)=>item.id!==id);
     saveToStorage(storageKey,shoppingList)
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
     setShoppingList(newShoppingList);
   }
   
@@ -64,6 +73,7 @@ export default function App() {
       return item;
     })
     saveToStorage(storageKey,newShoppingList)
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
     setShoppingList(newShoppingList);
   }
   return (
